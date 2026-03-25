72a296e185d709fa81f404d28cfad8d4cef2ba80
MarcosDurv <marcos.durvall@outlook.com>
2026-03-25 17:12:50 -0300

    feat:redirecting-routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import conectDB from './config/dbConect.js';
-import livro from './models/Livro.js';
+import routes from './routes/index.js';
 
 const conection = await conectDB();
 
@@ -13,17 +13,7 @@ conection.once("open", () => {
 });
 
 const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.status(200).send('Curso de express API');
-});
-
-// app.get("/livros", async (req, res) => {
-//     const listaLivros = await livro.find({});
-//     res.status(200).json(listaLivros);
-// });
+routes(app);
 
 app.get("/livros/:id", (req, res) => {
     const index = getLivro(req.params.id);
@@ -32,7 +22,6 @@ app.get("/livros/:id", (req, res) => {
 
 app.post("/livros", (req, res) => {
     livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
 });
 
 app.put("/livros/:id", (req, res) => {
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getLivro } from "../controllers/livrosController.js";
 import livroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
 routes.get("/livros", livroController.listarLivros);
+
+export default routes;
